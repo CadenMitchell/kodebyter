9dca8f56f17bf74c7efce81500127bfc4b88f26d
CadenMitchell <sidedvirus@outlook.com>
2018-07-29 16:38:47 -0600

    Update to Python3

--- kodebyter.py
+++ kodebyter.py
@@ -13,7 +13,7 @@ def caesar(rotate_string, number_to_rotate_by):
 	upper = ''.join(list(upper))
 	lower = ''.join(list(lower))
 
-	return rotate_string.translate(string.maketrans(string.ascii_uppercase, upper)).translate(string.maketrans(string.ascii_lowercase, lower))
+	return rotate_string.translate(str.maketrans(string.ascii_uppercase, upper)).translate(str.maketrans(string.ascii_lowercase, lower))
 
 print("Welcome to Kode Byter!")
 
@@ -26,59 +26,59 @@ print("Press Ctrl+Shift+V to paste")
 print("Will exit in 5")
 
 print("Enter message:")
-message = raw_input()
+message = input()
 
 print("Enter rotation:")
-rotation = raw_input()
+rotation = input()
 
-print str(rotation)
-print caesar(message, int(rotation))
+print(rotation)
+print(caesar(message, int(rotation)))
 
 print("Will exit in 4")
 
 print("Enter message:")
-message = raw_input()
+message = input()
 
 print("Enter rotation:")
-rotation = raw_input()
+rotation = input()
 
-print str(rotation)
-print caesar(message, int(rotation))
+print(rotation)
+print(caesar(message, int(rotation)))
 
 print("Will exit in 3")
 
 print("Enter message:")
-message = raw_input()
+message = input()
 
 print("Enter rotation:")
-rotation = raw_input()
+rotation = input()
 
-print str(rotation)
-print caesar(message, int(rotation))
+print(rotation)
+print(caesar(message, int(rotation)))
 
 print("Will exit in 2")
 
 print("Enter message:")
-message = raw_input()
+message = input()
 
 print("Enter rotation:")
-rotation = raw_input()
+rotation = input()
 
-print str(rotation)
-print caesar(message, int(rotation))
+print(rotation)
+print(caesar(message, int(rotation)))
 
 print("Will exit in 1")
 
 print("Enter message:")
-message = raw_input()
+message = input()
 
 print("Enter rotation:")
-rotation = raw_input()
+rotation = input()
 
-print str(rotation)
-print caesar(message, int(rotation))
+print(rotation)
+print(caesar(message, int(rotation)))
 
 print("Will exit in 0")
 
 print("Press enter to exit...")
-exit = raw_input()
+exit = input()
